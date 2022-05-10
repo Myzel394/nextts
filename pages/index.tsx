dd8f1ea91014d6e42bf29e1db54a753c417f9291
myzel394 <50424412+Myzel394@users.noreply.github.com>
2022-05-10 15:17:30 +0200

    bugfixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,16 +28,34 @@ export default function Home({
 
 
 export const getStaticProps = async (): Promise<GetStaticPropsResult<HomeProps>> => {
-    const [downloadsThisWeek, typesAvailable] = await Promise.allSettled([
-        getPackageDownloads("typescript"), getTypescriptTypesAmount(),
-    ]);
+    const [downloadsThisWeek, typesAvailable] = await (() => {
+        if (process.env.NODE_ENV !== "production") {
+            return Promise.allSettled([
+                ({
+                    value: 100,
+                }),
+                ({
+                    value: 100,
+                }),
+            ]);
+        }
+
+        return Promise.allSettled([
+            getPackageDownloads("typescript"), getTypescriptTypesAmount(),
+        ]);
+    })();
+
     if (downloadsThisWeek.status === "rejected" || typesAvailable.status === "rejected") {
         throw new Error(`Error while fetching data. downloadsThisWeek: ${downloadsThisWeek.status}, typesAvailable: ${typesAvailable.status}`);
     }
 
     return {
         props: {
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
             downloadsThisWeek: downloadsThisWeek.value,
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
             typesAvailable: typesAvailable.value,
             // According to latest Stackoverflow survey: https://insights.stackoverflow.com/survey/2020#technology-most-loved-dreaded-and-wanted-languages
             likePercentage: 67,
--- utils/getPackageDownloads.ts
+++ utils/getPackageDownloads.ts
@@ -3,7 +3,7 @@ import {promisify} from "util";
 import stats from "download-stats";
 
 const getPackageDownloads = async (packageName: string): Promise<number> => {
-    const result = await promisify(stats.get.lastWeek)("typescript");
+    const result = await promisify(stats.get.lastWeek)(packageName);
 
     return result.downloads;
 };
